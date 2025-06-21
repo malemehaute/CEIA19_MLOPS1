2cc2e6fbbd70e287e68559e9afe9324aa7bdade5
Martín Errázquin <errazquinmartin@gmail.com>
2025-06-21 01:54:52 -0300

    Reworked ETL DAG

--- airflow/dags/etl_process.py
+++ airflow/dags/etl_process.py
@@ -1,15 +1,20 @@
 import datetime
-import pandas as pd
 from airflow.decorators import dag, task
 
 markdown_text = """
 ### ETL Process for Podcast Data
 
 This DAG extracts information from the original podcast CSV file.
-It preprocesses the data by creating dummy variables and scaling numerical features.
+Preprocessing consists of:
+1. Dropping duplicated rows
+2. Making a relative target
+3. Encoding ordinal categories as numbers
+4. Filling some columns with NAs
+5. Dropping remaining rows with NAs (non-NA-supported columns)
+6. Dropping irrelevant/redundant columns
 
-After preprocessing, the data is saved back into a S3 bucket as two separate CSV files: one for training and one for
-testing. The split between the training and testing datasets is 70/30 and they are stratified.
+After preprocessing, the data is split in 70-train/30-test subsets 
+and saved back into a S3 bucket as two separate CSV files.
 """
 
 default_args = {
@@ -47,20 +52,8 @@ def process_etl_podcast_data():
 
         data_path = "s3://data/raw/podcast.csv"
 
-        # DEBUG:
-        from pathlib import Path
-        print("<DEBUGGING>")
-        for p in Path(".").iterdir():
-            print(p)
-        print("</DEBUGGING>")
-
         # simulate downloading data
         dataframe = pd.read_csv("/podcast_dataset.csv")
-        #dataframe = pd.read_csv(r"C:\Users\Alejandro\Documents\GitHub\CEIA19_MLOPS1\podcast_dataset.csv")
-
-        # Assuming 'listen_count' is the target column in your podcast dataset.
-        # Please change this if your target column is different.
-        target_col = Variable.get("target_col_podcast", default_var="Listening_Time_minutes")
 
         wr.s3.to_csv(df=dataframe,
                      path=data_path,
@@ -68,16 +61,15 @@ def process_etl_podcast_data():
 
 
     @task.virtualenv(
-        task_id="make_dummies_variables",
+        task_id="preprocessing",
         requirements=["awswrangler==3.6.0"],
         system_site_packages=True
     )
-    def make_dummies_variables():
+    def preprocessing():
         """
-        Convert categorical variables into one-hot encoding.
+        Drop duplicated rows, encode ordinals, fill NAs, drop columns.
         """
         import json
-        import datetime
         import boto3
         import botocore.exceptions
         import mlflow
@@ -85,33 +77,50 @@ def process_etl_podcast_data():
         import awswrangler as wr
         import pandas as pd
         import numpy as np
+        import datetime
 
         from airflow.models import Variable
 
         data_original_path = "s3://data/raw/podcast.csv"
-        data_end_path = "s3://data/raw/podcast_dummies.csv"
-        dataset = wr.s3.read_csv(data_original_path)
+        data_end_path = "s3://data/raw/podcast_preproc.csv"
+        df_original = wr.s3.read_csv(data_original_path)
 
-        # Clean duplicates
-        dataset.drop_duplicates(inplace=True, ignore_index=True)
-        # Drop NaN
-        dataset.dropna(inplace=True, ignore_index=True)
+        # drop duplicates
+        df = df_original.drop_duplicates().copy()
 
-        # Assuming categorical columns need to be identified from the podcast dataset
-        # For example: categories_list = ["genre", "language"]
-        # Please update this list with the actual categorical columns in your podcast_dataset.csv
-        categories_list = dataset.select_dtypes(include='object').columns.tolist()
+        # relative target
+        target_abs_col = Variable.get('target_abs_col')
+        target_rel_col = Variable.get('target_rel_col')
+        df[target_rel_col] = df[target_abs_col] / df['Episode_Length_minutes']
 
-        dataset_with_dummies = pd.get_dummies(data=dataset,
-                                              columns=categories_list,
-                                              drop_first=True)
+        # episode number encoding as int
+        df['Episode_Number'] = df['Episode_Title'].apply(lambda x: x.split(" ")[-1]).astype(int)
 
-        wr.s3.to_csv(df=dataset_with_dummies,
+        # guest absence
+        df['no_guest'] = df['Guest_Popularity_percentage'].isna()
+
+        # NA imputation
+        df.fillna({'Guest_Popularity_percentage':-1}, inplace=True)
+
+        # drop rows with NAs on other columns
+        df.dropna(inplace=True)
+
+        # reset index
+        df.reset_index(inplace=True, drop=True)
+
+        # sentiment encoding
+        df['sentiment_encoded'] = df['Episode_Sentiment'].map({'Neutral':0, 'Negative':-1, 'Positive':1}).astype(int)
+
+        # drop redundant/irrelevant columns
+        df.drop(columns=['Episode_Title', 'Episode_Sentiment', target_abs_col], inplace=True)
+
+        wr.s3.to_csv(df=df,
                      path=data_end_path,
                      index=False)
 
         # Save information of the dataset
         client = boto3.client('s3')
+
 
         data_dict = {}
         try:
@@ -124,25 +133,35 @@ def process_etl_podcast_data():
                 # Something else has gone wrong.
                 raise e
 
-        target_col = Variable.get("target_col_podcast", default_var="Listening_Time_minutes")
-        dataset_log = dataset.drop(columns=target_col)
-        dataset_with_dummies_log = dataset_with_dummies.drop(columns=target_col)
+        df_original_log = df_original.drop(columns=target_abs_col)
+        df_log = df.drop(columns=target_rel_col)
 
         # Upload JSON String to an S3 Object
-        data_dict['columns'] = dataset_log.columns.to_list()
-        data_dict['columns_after_dummy'] = dataset_with_dummies_log.columns.to_list()
-        data_dict['target_col'] = target_col
-        data_dict['categorical_columns'] = categories_list
-        data_dict['columns_dtypes'] = {k: str(v) for k, v in dataset_log.dtypes.to_dict().items()}
-        data_dict['columns_dtypes_after_dummy'] = {k: str(v) for k, v in dataset_with_dummies_log.dtypes.to_dict().items()}
+        data_dict['target_abs_col'] = target_abs_col
+        data_dict['target_rel_col'] = target_rel_col
+        today_date = datetime.datetime.today().strftime('%Y/%m/%d-%H:%M:%S"')
+        data_dict['date'] = today_date
 
-        category_dummies_dict = {}
-        for category in categories_list:
-            category_dummies_dict[category] = np.sort(dataset_log[category].unique()).tolist()
+        for ds, suffix in (
+            (df_original_log, '_original'),
+            (df_log, '_after_preproc')
+        ):
+            data_dict['columns'+suffix] = ds.columns.to_list()
+            data_dict['categorical_columns'+suffix] = ds.columns[ds.dtypes=='object'].tolist()
 
-        data_dict['categories_values_per_categorical'] = category_dummies_dict
+            categories_list = {
+                k: str(v)
+                for k, v in ds.dtypes.to_dict().items()
+            }
 
-        data_dict['date'] = datetime.datetime.today().strftime('%Y/%m/%d-%H:%M:%S"')
+            data_dict['columns_dtypes'+suffix] = categories_list
+
+            data_dict['categories_values_per_categorical'+suffix] = {
+                category: np.sort(ds[category].unique()).tolist()
+                for category, dtype in categories_list.items()
+                if dtype == 'object'
+            }
+
         data_string = json.dumps(data_dict, indent=2)
 
         client.put_object(
@@ -150,25 +169,28 @@ def process_etl_podcast_data():
             Key='data_info/data.json',
             Body=data_string
         )
+        
 
         mlflow.set_tracking_uri('http://mlflow:5000')
         experiment = mlflow.set_experiment("Podcast Data")
 
-        mlflow.start_run(run_name='ETL_run_' + datetime.datetime.today().strftime('%Y/%m/%d-%H:%M:%S"'),
+        mlflow.start_run(run_name='ETL_run_' + today_date,
                          experiment_id=experiment.experiment_id,
                          tags={"experiment": "etl", "dataset": "Podcast data"},
                          log_system_metrics=True)
 
-        mlflow_dataset = mlflow.data.from_pandas(dataset,
+        mlflow_dataset = mlflow.data.from_pandas(df_original,
                                                  source="/podcast_dataset.csv",
-                                                 targets=target_col,
+                                                 targets=target_abs_col,
                                                  name="podcast_data_complete")
-        mlflow_dataset_dummies = mlflow.data.from_pandas(dataset_with_dummies,
+        
+        mlflow_dataset_preproc = mlflow.data.from_pandas(df,
                                                          source="/podcast_dataset.csv",
-                                                         targets=target_col,
-                                                         name="podcast_data_complete_with_dummies")
+                                                         targets=target_rel_col,
+                                                         name="podcast_data_complete_preprocessed")
+        
         mlflow.log_input(mlflow_dataset, context="Dataset")
-        mlflow.log_input(mlflow_dataset_dummies, context="Dataset")
+        mlflow.log_input(mlflow_dataset_preproc, context="Dataset")
 
     @task.virtualenv(
         task_id="split_dataset",
@@ -189,16 +211,16 @@ def process_etl_podcast_data():
                          path=path,
                          index=False)
 
-        data_original_path = "s3://data/raw/podcast_dummies.csv"
+        data_original_path = "s3://data/raw/podcast_preproc.csv"
         dataset = wr.s3.read_csv(data_original_path)
 
-        test_size = Variable.get("test_size_podcast", default_var=0.3)
-        target_col = Variable.get("target_col_podcast", default_var="Listening_Time_minutes")
+        test_size = Variable.get("test_size_podcast")
+        target_col = Variable.get("target_rel_col")
 
         X = dataset.drop(columns=target_col)
         y = dataset[[target_col]]
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=float(test_size), stratify=y)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=float(test_size))
 
 
         save_to_csv(X_train, "s3://data/final/train/podcast_X_train.csv")
@@ -206,83 +228,6 @@ def process_etl_podcast_data():
         save_to_csv(y_train, "s3://data/final/train/podcast_y_train.csv")
         save_to_csv(y_test, "s3://data/final/test/podcast_y_test.csv")
 
-    @task.virtualenv(
-        task_id="normalize_numerical_features",
-        requirements=["awswrangler==3.6.0",
-                      "scikit-learn==1.3.2",
-                      "mlflow==2.10.2"],
-        system_site_packages=True
-    )
-    def normalize_data():
-        """
-        Standardization of numerical columns
-        """
-        import json
-        import mlflow
-        import boto3
-        import botocore.exceptions
-
-        import awswrangler as wr
-        import pandas as pd
-
-        from sklearn.preprocessing import StandardScaler
-
-        def save_to_csv(df, path):
-            wr.s3.to_csv(df=df,
-                         path=path,
-                         index=False)
-
-        X_train = wr.s3.read_csv("s3://data/final/train/podcast_X_train.csv")
-        X_test = wr.s3.read_csv("s3://data/final/test/podcast_X_test.csv")
-
-        sc_X = StandardScaler(with_mean=True, with_std=True)
-        X_train_arr = sc_X.fit_transform(X_train)
-        X_test_arr = sc_X.transform(X_test)
-
-        X_train = pd.DataFrame(X_train_arr, columns=X_train.columns)
-        X_test = pd.DataFrame(X_test_arr, columns=X_test.columns)
-
-        save_to_csv(X_train, "s3://data/final/train/podcast_X_train.csv")
-        save_to_csv(X_test, "s3://data/final/test/podcast_X_test.csv")
-
-        # Save information of the dataset
-        client = boto3.client('s3')
-
-        try:
-            client.head_object(Bucket='data', Key='data_info/data.json')
-            result = client.get_object(Bucket='data', Key='data_info/data.json')
-            text = result["Body"].read().decode()
-            data_dict = json.loads(text)
-        except botocore.exceptions.ClientError as e:
-                # Something else has gone wrong.
-                raise e
-
-        # Upload JSON String to an S3 Object
-        data_dict['standard_scaler_mean'] = sc_X.mean_.tolist()
-        data_dict['standard_scaler_std'] = sc_X.scale_.tolist()
-        data_string = json.dumps(data_dict, indent=2)
-
-        client.put_object(
-            Bucket='data',
-            Key='data_info/data.json',
-            Body=data_string
-        )
-
-        mlflow.set_tracking_uri('http://mlflow:5000')
-        experiment = mlflow.set_experiment("Podcast Data")
-
-        # Obtain the last experiment run_id to log the new information
-        list_run = mlflow.search_runs([experiment.experiment_id], output_format="list")
-
-        with mlflow.start_run(run_id=list_run[0].info.run_id):
-
-            mlflow.log_param("Train observations", X_train.shape[0])
-            mlflow.log_param("Test observations", X_test.shape[0])
-            mlflow.log_param("Standard Scaler feature names", sc_X.feature_names_in_)
-            mlflow.log_param("Standard Scaler mean values", sc_X.mean_)
-            mlflow.log_param("Standard Scaler scale values", sc_X.scale_)
-
-
-    get_data() >> make_dummies_variables() >> split_dataset() >> normalize_data()
+    get_data() >> preprocessing() >> split_dataset()
 
 dag = process_etl_podcast_data()
